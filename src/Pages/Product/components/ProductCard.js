5d0e65cd56850bfcfb673aaf3f10d624bd83d06c
Andy Lin <ld130101@gmail.com>
2022-11-12 17:29:59 +0800

    取得商品資料

--- src/Pages/Product/components/ProductCard.js
+++ src/Pages/Product/components/ProductCard.js
@@ -1,12 +1,51 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function ProductCard() {
+  const [product, setProduct] = useState({
+    sid: 0,
+    name: '',
+    category: 0,
+    img: '',
+    price: 0,
+    member: 0,
+    member_price: 0,
+    specials: '',
+    info: '',
+    created_at: '',
+    inventory: 0,
+    on_sale: 1,
+  })
+  // 取得商品資料
+  const getProducts = async () => {
+    try {
+      const res = await axios.get('http://localhost:6001/product/p-json');
+
+      // console.log(res.data.rows);
+      const products = res.data.rows;
+      setProduct(products);
+
+    } catch (e) {
+      console.log(e.message);
+    }
+
+  }
+  // didMount 載入資料
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log(product);
+
+
+
   return (
     <>
-      <div class="pro-card">
-        <div class="img-wrap">
-          <img src="./images/product_1.png" alt="" />
+      <div className="pro-card">
+        <div className="img-wrap">
+          <img src="/images/test/can1.jpg" alt="" />
         </div>
-        <div class="pro-title">
+        <div className="pro-title">
           <p>寵物商品</p>
           <p><s>$1,280</s> <span>$980</span></p>
         </div>
