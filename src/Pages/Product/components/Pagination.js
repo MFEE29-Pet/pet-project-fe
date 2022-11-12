6d622769902135950e0fd3843a7068724f9a1e6e
Andy Lin <ld130101@gmail.com>
2022-11-13 01:25:13 +0800

    頁數切換

--- src/Pages/Product/components/Pagination.js
+++ src/Pages/Product/components/Pagination.js
@@ -9,7 +9,7 @@ export default function Pagination({ totalPages, page, usp }) {
       <div className="page">
         <ul>
           <li>
-            <Link to="/product?page=1">
+            <Link to={`/product?page=${+usp > 1 ? +usp - 1 : 1}`}>
               <i className="fa-solid fa-angle-left"></i>
             </Link>
           </li>
@@ -17,24 +17,23 @@ export default function Pagination({ totalPages, page, usp }) {
             Array(totalPages)
               .fill(1)
               .map((e, i) => {
-                // console.log(usp.split('=')[1]);
+                // console.log(usp);
                 // const p = page + i;
                 // console.log(p);
+                // console.log(usp);
                 // if (p < 1 || p > totalPages) return null;
                 return (
                   <li key={i}>
                     <Link
                       to={`/product?page=${i + 1}`}
-                      className={+usp.split('=')[1] === i + 1 ? 'active' : ''}
+                      className={+usp === i + 1 ? 'active' : ''}
                     >
                       <i
                         className={`fa-duotone ${
                           mode === 'dog' ? 'fa-bone' : 'fa-fish'
                         } text_main_light_color1`}
                         style={{
-                          display: `${
-                            +usp.split('=')[1] === i + 1 ? 'block' : 'none'
-                          }`,
+                          display: `${+usp === i + 1 ? 'block' : 'none'}`,
                         }}
                       ></i>
                       {i + 1}
@@ -43,9 +42,11 @@ export default function Pagination({ totalPages, page, usp }) {
                 );
               })}
           <li>
-            <a href="">
+            <Link
+              to={`/product?page=${+usp < totalPages ? +usp + 1 : totalPages}`}
+            >
               <i className="fa-solid fa-angle-right"></i>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
