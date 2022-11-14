a48c77190c2927296e94540f75b307356eff90c9
Kunda <love60066@gmail.com>
2022-11-14 09:16:45 +0800

    photo_transition

--- src/Pages/Index/Photo.js
+++ src/Pages/Index/Photo.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import PhotographerOne from './PhotographerOne';
 import PhotographerTwo from './PhotographerTwo';
 import PhotographerThree from './PhotographerThree';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const PHOTO = styled.div`
   width: 100%;
@@ -22,9 +25,26 @@ const PhotoBox = styled.div`
 `;
 
 function Photo() {
+  const PhotoMove = useRef();
+
+  useEffect(() => {
+    let container = document.querySelector(".bbb");
+    let t2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.aaa',
+        end: () => container.scrollWidth - document.documentElement.clientWidth,
+        scrub: true,
+        pin: true,
+      },
+      defaults: { ease: 'none'},
+    });
+
+    t2.to('.bbb', { x: '-66%' });
+  }, []);
+
   return (
-    <PHOTO>
-      <PhotoBox className="index_photo_bg">
+    <PHOTO className="aaa">
+      <PhotoBox className="index_photo_bg bbb" ref={PhotoMove} >
         <PhotographerOne />
         <PhotographerTwo />
         <PhotographerThree />
