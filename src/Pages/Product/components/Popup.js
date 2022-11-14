36c9369a671ca9035d3dec4941206f4728e773bb
AndyLin <ld130101@gmail.com>
2022-11-14 11:11:29 +0800

    自動填入和reset

--- src/Pages/Product/components/Popup.js
+++ src/Pages/Product/components/Popup.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 
 function Popup({ trigger, setTrigger }) {
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+
   return trigger ? (
     <>
-      <section class="popup">
-        <div class="filter-popup">
-          <div class="filter-title">
-            <i class="fa-solid fa-caret-right"></i>
+      <section className="popup">
+        <div className="filter-popup">
+          <div className="filter-title">
+            <i className="fa-solid fa-caret-right"></i>
             <h2>價格範圍</h2>
             <div
-              class="close"
+              className="close"
               onClick={() => {
                 setTrigger(!trigger);
               }}
@@ -17,52 +20,94 @@ function Popup({ trigger, setTrigger }) {
               關閉
             </div>
           </div>
-          <div class="price_input_form">
+          <div className="price_input_form">
             <form method="get">
-              <div class="price_input">
-                <label for="min_price"></label>
+              <div className="price_input">
+                <label htmlFor="min_price"></label>
                 <input
                   type="number"
                   min="0"
                   placeholder="最小值"
                   name="min_price"
                   id="min_price"
+                  value={minPrice}
+                  onChange={(e) => {
+                    setMinPrice(e.target.value);
+                  }}
                 />
                 <span> &#9473; </span>
-                <label for="max_price"></label>
+                <label htmlFor="max_price"></label>
                 <input
                   type="number"
                   placeholder="最大值"
                   name="max_price"
                   id="max_price"
+                  value={maxPrice}
+                  onChange={(e) => {
+                    setMaxPrice(e.target.value);
+                  }}
                 />
               </div>
-              <div class="btn-group">
+              <div className="btn-group">
                 {/* <!-- 使用自動填入 --> */}
-                <button class="active">0-500</button>
-                <button>500-1000</button>
-                <button>1000-1500</button>
+                <button
+                  type="button"
+                  className="active"
+                  onClick={() => {
+                    setMinPrice(0);
+                    setMaxPrice(500);
+                  }}
+                >
+                  0-500
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setMinPrice(500);
+                    setMaxPrice(1000);
+                  }}
+                >
+                  500-1000
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setMinPrice(1000);
+                    setMaxPrice(1500);
+                  }}
+                >
+                  1000-1500
+                </button>
               </div>
-              {/* <!-- <button class="price_filter_submit">確定</button>
-              <button class="price_filter_reset">重設</button> --> */}
+              {/* <!-- <button className="price_filter_submit">確定</button>
+              <button className="price_filter_reset">重設</button> --> */}
             </form>
           </div>
-          <div class="filter-title">
-            <i class="fa-solid fa-caret-right"></i>
+          <div className="filter-title">
+            <i className="fa-solid fa-caret-right"></i>
             <h2>服務與促銷</h2>
           </div>
-          <div class="service_sale_form">
+          <div className="service_sale_form">
             <form method="get">
               {/* <!-- 被選取的話增加class active --> */}
-              <label for="sale" class="active">
+              <label htmlFor="sale" className="active">
                 特價促銷
               </label>
               <input type="radio" name="sale" id="sale" />
-              <label for="multi">多件優惠</label>
+              <label htmlFor="multi">多件優惠</label>
               <input type="radio" name="sale" id="multi" />
 
-              <button class="price_filter_submit">確定</button>
-              <button class="price_filter_reset">重設</button>
+              <button className="price_filter_submit">確定</button>
+              <button
+                className="price_filter_reset"
+                type="button"
+                onClick={() => {
+                  setMinPrice(0);
+                  setMaxPrice(0);
+                }}
+              >
+                重設
+              </button>
             </form>
           </div>
         </div>
