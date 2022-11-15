2777d9237398248820e506f489fa6e76d4ff5807
pinwei <pg2017fly@gmail.com>
2022-11-15 22:40:53 +0800

    MemberReact

--- src/Pages/Member/Member.js
+++ src/Pages/Member/Member.js
@@ -1,5 +1,33 @@
+import './Member.css';
+import MemberShipLeft from './MemberShipLeft';
+import MemberLevel from './MemberLevel.js';
+import MemberPetAdd from './MemberPetAdd';
+import MemberArticleCollect from './MemberArticleCollect';
+import MemberAppointment from './MemberAppointment';
+import Breadcrumb from '../../Components/breadcrumb/Breadcrumb';
+import { useState } from 'react';
+
 function Member() {
-  return <div>Member</div>;
+  const components = [
+    MemberLevel,
+    MemberPetAdd,
+    MemberArticleCollect,
+    MemberAppointment,
+  ];
+  const [pageIndex, setPageIndex] = useState(0);
+  const Nowcomponents = components[pageIndex];
+  return (
+    <>
+      <div className="member">
+        <MemberShipLeft
+          totalPage={components}
+          pageIndex={pageIndex}
+          setPageIndex={setPageIndex}
+        />
+        <Nowcomponents />
+      </div>
+    </>
+  );
 }
 
 export default Member;
---
+++ src/Pages/Member/MemberAppointment.js
@@ -0,0 +1,223 @@
+import './Member.css';
+function MemberAppointment() {
+  return (
+    <>
+      <div className="Hospital">
+        <div className="Hospital-all">
+          <div className="Hospital-order">
+            <h3>進行中</h3>
+            <h3>取消預約</h3>
+          </div>
+          <div className="Hospital-info">
+            <h2>診所資訊</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>診所</h5>
+
+                <span>暖陽寵物照護中心</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>地址</h5>
+
+                <span>116文山區景美街15號</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>電話</h5>
+
+                <span>(02)2328-5915</span>
+              </div>
+            </div>
+          </div>
+          <div className="time">
+            <h2>預約時間</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>日期</h5>
+                <span>2022/12/14</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>時段</h5>
+                <span>下午</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>序號</h5>
+                <span>06</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Hospital-all">
+          <div className="Hospital-order">
+            <h3>進行中</h3>
+            <h3>取消預約</h3>
+          </div>
+          <div className="Hospital-info">
+            <h2>診所資訊</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>診所</h5>
+
+                <span>暖陽寵物照護中心</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>地址</h5>
+
+                <span>116文山區景美街15號</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>電話</h5>
+
+                <span>(02)2328-5915</span>
+              </div>
+            </div>
+          </div>
+          <div className="time">
+            <h2>預約時間</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>日期</h5>
+                <span>2022/12/14</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>時段</h5>
+                <span>下午</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>序號</h5>
+                <span>06</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Hospital-all">
+          <div className="Hospital-order">
+            <h3>已完成</h3>
+          </div>
+          <div className="Hospital-info">
+            <h2>診所資訊</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>診所</h5>
+
+                <span>暖陽寵物照護中心</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>地址</h5>
+
+                <span>116文山區景美街15號</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>電話</h5>
+
+                <span>(02)2328-5915</span>
+              </div>
+            </div>
+          </div>
+          <div className="time">
+            <h2>預約時間</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>日期</h5>
+                <span>2022/12/14</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>時段</h5>
+                <span>下午</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>序號</h5>
+                <span>06</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Hospital-all">
+          <div className="Hospital-order">
+            <h3>已完成</h3>
+          </div>
+          <div className="Hospital-info">
+            <h2>診所資訊</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>診所</h5>
+
+                <span>暖陽寵物照護中心</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>地址</h5>
+
+                <span>116文山區景美街15號</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>電話</h5>
+
+                <span>(02)2328-5915</span>
+              </div>
+            </div>
+          </div>
+          <div className="time">
+            <h2>預約時間</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>日期</h5>
+                <span>2022/12/14</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>時段</h5>
+                <span>下午</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>序號</h5>
+                <span>06</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Hospital-all">
+          <div className="Hospital-order">
+            <h3>已完成</h3>
+          </div>
+          <div className="Hospital-info">
+            <h2>診所資訊</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>診所</h5>
+
+                <span>暖陽寵物照護中心</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>地址</h5>
+
+                <span>116文山區景美街15號</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>電話</h5>
+
+                <span>(02)2328-5915</span>
+              </div>
+            </div>
+          </div>
+          <div className="time">
+            <h2>預約時間</h2>
+            <div className="Hospital-details">
+              <div className="Hospital-A">
+                <h5>日期</h5>
+                <span>2022/12/14</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>時段</h5>
+                <span>下午</span>
+              </div>
+              <div className="Hospital-A">
+                <h5>序號</h5>
+                <span>06</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MemberAppointment;
