005fe9b9b032a2c892017f2383800eca71752aec
Kunda <love60066@gmail.com>
2022-11-16 11:09:20 +0800

    clinic_add_breadcrumb

--- src/Pages/Clinic/Clinic.js
+++ src/Pages/Clinic/Clinic.js
@@ -3,12 +3,21 @@ import ClinicSelect from './components/ClinicSelect';
 import ClinicMap from './components/ClinicMap';
 import styled from 'styled-components';
 import Breadcrumb from '../../Components/breadcrumb/Breadcrumb';
+import BreadcrumbRightArrowIcon from '../../Components/breadcrumb/BreadcrumbRightArrowIcon';
 
 const ClinicBox = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+`;
+
+const BreadcrumbBox = styled.div`
+  width: 1200px;
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom:50px;
 `;
 
 const ClinicBoxContainer = styled.div`
@@ -26,12 +35,27 @@ const ListBox = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
+const Clinicroutes = [
+  {
+    to: '/',
+    label: '首頁',
+  },
+  {
+    to: '/clinic',
+    label: '醫療診所',
+  },
+];
 
 function Clinic() {
   const [dataFromSelect, setDataFromSelect] = useState([]);
   return (
     <ClinicBox>
-      <Breadcrumb />
+      <BreadcrumbBox>
+        <Breadcrumb
+          routes={Clinicroutes}
+          separator={<BreadcrumbRightArrowIcon />}
+        />
+      </BreadcrumbBox>
       <ClinicBoxContainer>
         <ListBox>
           <ClinicSelect setDataFromSelect={setDataFromSelect} />
