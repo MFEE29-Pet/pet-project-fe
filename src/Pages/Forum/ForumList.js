a5498f9618c467ca6453a174942d65ed42c63126
Boyen <hedonist1216@gmail.com>
2022-11-16 19:01:07 +0800

    update ForumList 90%

--- src/Pages/Forum/ForumList.js
+++ src/Pages/Forum/ForumList.js
@@ -1,11 +1,20 @@
 import SearchBar from './components/SearchBar';
+import ButtonBar from './components/ButtonBar';
+import ForumListBar from './components/ForumListBar';
 
 function ForumList() {
   return (
     <>
       <SearchBar />
+      <ButtonBar />
+      <ForumListBar />
+      <ForumListBar />
+      <ForumListBar />
+      <ForumListBar />
+      <ForumListBar />
+      <ForumListBar />
     </>
   );
 }
 
-export default ForumList;
+export default ForumListBar;
