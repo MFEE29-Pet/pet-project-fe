0e545a2062a7e61e38c3187faf0de5ba7d27abb1
AndyLin <ld130101@gmail.com>
2022-11-17 19:25:46 +0800

    購物車數字切換顏色

--- src/Pages/Product/components/CartIcon.js
+++ src/Pages/Product/components/CartIcon.js
@@ -2,26 +2,28 @@ import { useContext } from 'react';
 import Badge from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import ProductDetailContext from '../../../contexts/ProductDetailContext';
+import SwitchButtonContext from '../../../contexts/SwitchButtonContext';
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
+const StyledBadge = styled(Badge)((props) => ({
   '& .MuiBadge-badge': {
     right: -3,
     top: 13,
     border: `2px solid #fff`,
     padding: '0 4px',
-    background:'#ea5514'
+    background: `${props.mode === 'dog' ? '#ea5514' : '#4c748e'}`,
   },
 }));
 
 export default function CustomizedBadges() {
-  const { amount, setAmount, cartAmount } = useContext(ProductDetailContext);
+  const { mode } = useContext(SwitchButtonContext);
+  const { cartAmount } = useContext(ProductDetailContext);
   console.log(cartAmount);
   return (
     <IconButton aria-label="cart">
-      <StyledBadge badgeContent={cartAmount} color="secondary">
-        <ShoppingCartIcon />
+      <StyledBadge badgeContent={cartAmount} mode={mode} color="secondary">
+        <ShoppingCartOutlinedIcon />
       </StyledBadge>
     </IconButton>
   );
