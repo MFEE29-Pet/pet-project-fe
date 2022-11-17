b9a4117627adb7f956ab81fcd39d210c39594323
Kunda <love60066@gmail.com>
2022-11-17 14:57:51 +0800

    新增cart連結

--- src/Components/Navbar/component/Cart.js
+++ src/Components/Navbar/component/Cart.js
@@ -1,20 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const I = styled.i`
   color: #c9caca;
   font-size: 20px;
 `;
 
-const CART = styled.div`
+const CART = styled(Link)`
   width: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
 `
 
 function Cart() {
   return (
-    <CART>
+    <CART to='/cart'>
       <I className="fa-light fa-cart-shopping"></I>
     </CART>
   );
