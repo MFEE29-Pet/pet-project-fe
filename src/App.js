882276333d58c41025d538aa8d4c8841748b3201
Andy Ln <ld130101@gmail.com>
2022-11-17 17:34:04 +0800

    member 新路由 先註解

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ProductDetail from './Pages/Product/ProductDetail';
 import Photographer from './Pages/Product/Photographer';
 import PhotographerForm from './Pages/Product/PhotographerForm';
 import AllContextProviders from './contexts/AllContextProviders';
-import MemberSing from './Pages/Member/MemberSing';
+// import MemberSing from './Pages/Member/MemberSing';
 
 function App() {
   // const [checked, setChecked] = useState(true);
@@ -56,7 +56,7 @@ function App() {
             <Route path="forum" element={<Forum />} />
 
             <Route path="member" element={<Member />} />
-            <Route path="member/memberShipAdd" element={<MemberSing />} />
+            {/* <Route path="member/memberShipAdd" element={<MemberSing />} /> */}
           </Routes>
         </AllContextProviders>
         <Footer />
