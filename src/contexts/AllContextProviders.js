13f40ef0c08e10f7e5b36a7ec32978c9c2bf5166
AndyLin <ld130101@gmail.com>
2022-11-17 19:25:30 +0800

    del CartInfoContext

--- src/contexts/AllContextProviders.js
+++ src/contexts/AllContextProviders.js
@@ -2,12 +2,10 @@ import { PageContextProvider } from '../Pages/Product/contexts/PageContext';
 import { ProductDataContextProvider } from './ProductDataContext';
 import { ProductDetailContextProvider } from './ProductDetailContext';
 import { StyledEngineProvider } from '@mui/material';
-import { CartInfoContextProvider } from './CartInfoContext';
 
 export default function AllContextProviders({ children }) {
   return (
     <ProductDataContextProvider>
-      <CartInfoContextProvider>
         <StyledEngineProvider>
           <PageContextProvider>
             <ProductDetailContextProvider>
@@ -15,7 +13,6 @@ export default function AllContextProviders({ children }) {
             </ProductDetailContextProvider>
           </PageContextProvider>
         </StyledEngineProvider>
-      </CartInfoContextProvider>
     </ProductDataContextProvider>
   );
 }
