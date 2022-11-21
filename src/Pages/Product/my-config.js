1d23dcc421a8c894da609595063708c9f46e5997
Andy Lin <ld130101@gmail.com>
2022-11-22 01:15:40 +0800

    攝影師預約表單

--- src/Pages/Product/my-config.js
+++ src/Pages/Product/my-config.js
@@ -3,3 +3,4 @@ export const PRODUCT_LIST = `${MY_HOST}/product/p-json`;
 export const PRODUCT_DETAIL = `${MY_HOST}/product/detail`;
 export const INSERT_REPLY = `${MY_HOST}/product/addReply-api`;
 export const PHOTOGRAPH = `${MY_HOST}/product/photographers-json`;
+export const PHOTOGRAPH_FORM = `${MY_HOST}/product/photographersForm`;
