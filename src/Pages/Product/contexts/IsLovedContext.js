c3ea8b59ded73c8e25e9e15d776b22d381b839f6
Andy Lin <ld130101@gmail.com>
2022-11-24 23:14:08 +0800

    debug

--- src/Pages/Product/contexts/IsLovedContext.js
+++ src/Pages/Product/contexts/IsLovedContext.js
@@ -21,7 +21,7 @@ export const IsLovedContextProvider = function ({ children }) {
 
   // 從localStorage取得 m_sid
 
-  const m_sid = JSON.parse(localStorage.getItem('auth')).sid
+  const m_sid = JSON.parse(localStorage.getItem('auth'))
     ? JSON.parse(localStorage.getItem('auth')).sid
     : '未登入';
   // console.log(m_sid);
