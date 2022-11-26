c8879ef249bd5629b44346709db99dd575fb180f
Andy Lin <ld130101@gmail.com>
2022-11-26 22:44:42 +0800

    刪註解

--- src/Pages/Product/ProductDetail.js
+++ src/Pages/Product/ProductDetail.js
@@ -457,30 +457,6 @@ function ProductDetail() {
       <ReplyPopup setShowDiv={setShowDiv} showDiv={showDiv} sid={data.sid} />
       {/* <!-- history sec --> */}
       <History />
-      {/* <section className="history">
-        <div className="history-side-div">
-          <div className="div-title-seen">
-            <h2>最近看過</h2>
-          </div>
-          <div className="div-product-seen">
-            <div className="product-img-wrap">
-              <Link href="">
-                <img src="/images/test/can1.jpg" alt="" />
-              </Link>
-            </div>
-            <div className="product-img-wrap">
-              <Link href="">
-                <img src="/images/test/can1.jpg" alt="" />
-              </Link>
-            </div>
-            <div className="product-img-wrap">
-              <Link href="">
-                <img src="/images/test/can1.jpg" alt="" />
-              </Link>
-            </div>
-          </div>
-        </div>
-      </section> */}
 
       {/* 相關商品 */}
       <section className="bottom-pro-list">
