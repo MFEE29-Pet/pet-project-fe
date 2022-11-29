379121c852276494c2a6efa063ed38e24ae49b49
Kunda <love60066@gmail.com>
2022-11-30 00:49:45 +0800

    更改會員中心大頭照

--- src/Pages/Member/MemberForgrtPassword.js
+++ src/Pages/Member/MemberForgrtPassword.js
@@ -1,43 +1,55 @@
-import React from 'react'
+import React from 'react';
 import './Member.css';
 function MemberForgrtPassword() {
   return (
     <div className="pagePassword">
-                <div className="enter-A">
-                    <h2>目前的密碼</h2>
-                    <div className="enter-C">
-                        <div className="input">
-                            
-                            <input type="text"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="enter-A">
-                    <h2>新密碼</h2>
-                    <div className="enter-B">
-                        <div>
-                           
-                            <input type="text"/>
-                        </div>
-                        
-                           
-                    </div>
-                </div>
-                <div className="enter-A">
-                    <h2>新密碼確認</h2>
-                    <div className="enter-B">
-                        <div>
-                            
-                            <input type="text"/>
-                        </div>
-                        
-                           
-                    </div>
-                </div>
-                <button className="button">確認</button>
-                
-            </div>
-  )
+      <div className="enter-A">
+        <h2>目前的密碼</h2>
+        <div className="enter-C">
+          <div className="input">
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <div className="enter-A">
+        <h2>新密碼</h2>
+        <div className="enter-B" style={{ backgroundColor: 'transparent' }}>
+          <div>
+            <input
+              type="text"
+              style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                outline: 'none',
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="enter-A">
+        <h2>新密碼確認</h2>
+        <div className="enter-B" style={{ backgroundColor: 'transparent' }}>
+          <div>
+            <input
+              type="text"
+              style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                outline: 'none',
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="" style={{display:'flex',justifyContent:'center'}}>
+        <button
+          className="bg_main_light_color1" style={{padding:'10px 20px',border:'none',fontSize:'16px',color:'#fff',borderRadius:'15px'}}
+        >
+          儲存
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default MemberForgrtPassword
+export default MemberForgrtPassword;
