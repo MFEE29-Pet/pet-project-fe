6951ebe1d4de6fec951999bad228fcb5d9981b88
AndyLin <ld130101@gmail.com>
2022-11-29 18:39:08 +0800

    socket io 未完成

--- src/Pages/Product/components/Socket/Socket.js
+++ src/Pages/Product/components/Socket/Socket.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState, useContext } from 'react';
 import webSocket from 'socket.io-client';
 import { SOCKET_HOST } from '../../my-config';
 import AuthContext from '../../../../contexts/AuthContext';
+import { useNavigate } from 'react-router';
+import dayjs from 'dayjs';
 
 const socket = webSocket.connect(`${SOCKET_HOST}`);
 
@@ -61,63 +63,54 @@ function Socket() {
   // room state
   const [room, setRoom] = useState('');
 
+  // username
+  const [username, setUsername] = useState('');
+
   // messages state
   const [message, setMessage] = useState('');
-  const [messageReceived, setMessageReceived] = useState('');
+  const [messageReceived, setMessageReceived] = useState([]);
+
+  const navigate = useNavigate();
 
   const joinRoom = () => {
-    if (room !== '') {
-      socket.emit('join_room', room);
+    if (room !== '' && username !== '') {
+      socket.emit('join_room', { username, room });
     }
+    navigate('/chat');
   };
 
   const sendMessage = () => {
     socket.emit('send_message', { message, room });
+    // console.log(message);
+    // console.log(messageReceived);
   };
-
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setUsername(JSON.parse(localStorage.getItem('auth')).name);
+    }
+    // console.log({ username });
+  }, []);
   useEffect(() => {
     socket.on('receive_message', (data) => {
-      setMessageReceived(data.message);
+      console.log(data);
+      setMessageReceived((state) => [
+        ...state,
+        {
+          message: data.message,
+          username: data.username,
+          __createdtime__: data.__createdtime__,
+        },
+      ]);
     });
+
+    return () => socket.off('receive_message');
   }, [socket]);
+
 
   return (
     <>
       <div className="socket">
-        {/* 新增下拉是選單選擇房間 */}
-        {/* <select
-          name=""
-          id=""
-          onChange={(e) => {
-            setRoom(e.target.value);
-          }}
-        >
-          <option value="">請選擇房間</option>
-          <option value="room1">Room1</option>
-          <option value="room2">Room2</option>
-        </select>
-        <input type="button" value={`連線`} onClick={connectWebSocket} />
-        <input
-          type="button"
-          value={`送出訊息，只有自己收到回傳`}
-          onClick={() => {
-            sendMessage('getMessage');
-          }}
-        />
-        <input
-          type="button"
-          value={`送出訊息，讓所有人收到回傳`}
-          onClick={() => {
-            sendMessage('getMessageAll');
-          }}
-        />
-        <input
-          type="button"
-          value={`送出訊息，除了自己外所有人收到回傳`}
-          onClick={() => {
-            sendMessage('getMessageLess');
-          }}
-        /> */}
+        <input type="text" defaultValue={username || '未登入'} hidden />
         <input
           type="text"
           placeholder="Room number"
@@ -135,7 +128,9 @@ function Socket() {
         />
         <button onClick={sendMessage}>送出</button>
         <h1> Message</h1>
-        {messageReceived}
+        <div className="message_socket">
+          <p>{messageReceived}</p>
+        </div>
       </div>
     </>
   );
