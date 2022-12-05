1563314f5a5e4995a21a42323798b42596355899
Andy Ln <ld130101@gmail.com>
2022-12-05 18:03:49 +0800

    messages sender photo

--- src/Pages/Product/components/OnlineService/components/Messages.js
+++ src/Pages/Product/components/OnlineService/components/Messages.js
@@ -5,12 +5,16 @@ const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
 
 function Messages({ own, message }) {
-  // console.log(message);
+  console.log(message);
   return (
     <>
       <div className={own ? 'message own' : 'message'}>
         <div className="messagesTop">
-          <img className="messageImg" src="/images/test/user1.jpeg" alt="" />
+          <img
+            className="messageImg"
+            src={`http://localhost:6001/uploads/imgs/${message.senderImg}`}
+            alt=""
+          />
 
           <div
             className="wrap"
